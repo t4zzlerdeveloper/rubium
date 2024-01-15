49b2f32cc00da2fc889949eff11dc1918ed848c3
t4zzlerdeveloper <38925744+t4zzlerdeveloper@users.noreply.github.com>
2024-01-15 18:44:08 +0000

    Simple verify endpoint added

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import RequireAuth from "./views/RequireAuth";
 import LoadingPage from "./pages/LoadingPage";
 import PublishedNote from "./pages/PublishedNote";
 import RegisterPage from "./pages/RegisterPage";
+import VerifyPage from "./pages/VerifyPage";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
                 <Route index element={<LandingPage/>} />
                 <Route path="login" element={<LoginPage />} />
                 <Route path="register" element={<RegisterPage />} />
+                <Route path="verify" element={<VerifyPage/>} />
                 <Route path="app" element={<RequireAuth><NoteApp/></RequireAuth>} />
                 <Route path="note" element={<PublishedNote/>}>
                   <Route path="*" element={<PublishedNote/>}/>
---
+++ src/pages/VerifyPage/index.jsx
@@ -0,0 +1,31 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { account } from '../../lib/appwrite';
+
+function VerifyPage(){
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const [text,setText] = useState("Verifying...");
+
+    useEffect(()=>{
+        const userId = searchParams.get("userId")
+        const secret = searchParams.get("secret")
+
+        account.updateVerification(userId,secret)
+        .then((res)=>{
+            setText("Verified Sucessfully! Redirecting...")
+            navigate("/app");
+        })
+        .catch(()=>{
+            setText("This link is invalid or has expired!")
+        })
+    },[])
+
+    return (<>
+        {text}
+    </>)
+}
+
+export default VerifyPage;
