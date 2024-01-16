83a545673d1b0c992a89e8d4f6aa633282acf9b7
t4zzlerdeveloper <38925744+t4zzlerdeveloper@users.noreply.github.com>
2024-01-16 15:00:09 +0000

    Landing Page Stable Version

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 Rubium is a Secure, Fast and Open-Source Note-taking app, an alternative to other ridiculously-expensive apps. 
 
-  <img style="object-fit:contain" height="500" src="/src/assets/screenshots/login-page.png"/>
+  <img style="object-fit:contain" height="500" src="/src/assets/screenshots/landing.png"/>
 
 #### Proudly built with Appwrite
 
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -5,9 +5,17 @@ import star from '../../assets/star.svg'
 import arrow from '../../assets/arrow-down.svg'
 import shield from '../../assets/encrypted.svg'
 import money from '../../assets/paid.svg'
+
+import github from '../../assets/socials/github.svg'
+import appwrite from '../../assets/socials/appwrite.svg'
+import linkedin from '../../assets/socials/linkedin.svg'
+
 import screenshotApp from '../../assets/screenshots/login-page.png'
 import screenshotNote from '../../assets/screenshots/note-page.png'
 import './LandingPage.css'
+
+const DEV_GITHUB = "https://github.com/t4zzlerdeveloper";
+const RUBIUM_GITHUB = DEV_GITHUB + "/rubium";
 
 function LandingPage(){
 
@@ -23,7 +31,7 @@ function LandingPage(){
             <a>Suggestions</a>
         </div>
         <div className='nav-right'>
-            <button className='star-git' onClick={()=>{window.open("https://github.com/t4zzlerdeveloper/rubium")}}>
+            <button className='star-git' onClick={()=>{window.open(RUBIUM_GITHUB)}}>
                 <img src={star}/>
                 <p>Star on GitHub</p>
             </button>
@@ -42,36 +50,39 @@ function LandingPage(){
 
     
     </div>
-    
+
     <div className='screenshot-app'>
         <div className='scr-app-div'>
             <img src={shield} />
             <h2><strong>Secure Authentication</strong> powered by Appwrite</h2>
         </div>
-        <img src={screenshotApp}/>
+        <img src={screenshotApp} onClick={()=>{navigate("/login")}}/>
      </div>
 
      <div className='screenshot-app invert-src'>
-     <img src={screenshotNote}/>
+     <img src={screenshotNote} onClick={()=>{navigate("/app")}}/>
         <div className='scr-app-div'>
             <img src={money} />
             <h2><strong>Free of charge</strong><br></br> Notes already cost your time.</h2>
         </div>
      </div>
 
-    <div>
-        <h4>Subscribe to our newsletter</h4>
-        <input placeholder='Your email'></input>
-    </div>
+    
 
     <footer>
+        <div className='newsletter'>
+            <h4>Subscribe to our newsletter</h4>
+            <p>Get updates on your email about new and cool features.</p>
+            <input placeholder='Your email'></input>
+            <button enabled="false">Subscribe</button>
+        </div>
         <div className='socials'>
-            <img src={rubiumLogo}/>
-            <img src={rubiumLogo}/>
-            <img src={rubiumLogo}/>
+            <img src={github} onClick={()=>{window.open(RUBIUM_GITHUB)}}/>
+            <img src={appwrite}/>
+            <img src={linkedin}/>
         </div>
         <p className='copyright'>
-            &copy; 2024 Rubium. All rights reserved.
+            &copy; {new Date().getFullYear() || "2024"} Rubium&nbsp;&nbsp;|&nbsp;&nbsp;Developed by <a onClick={()=>{window.open(DEV_GITHUB)}}> @t4zzlerdeveloper</a>
         </p>
     </footer>
 
