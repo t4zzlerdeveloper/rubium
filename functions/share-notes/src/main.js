f45a82f2553a7b379514b655e46930434bd0e38a
t4zzlerdeveloper <38925744+t4zzlerdeveloper@users.noreply.github.com>
2024-01-17 16:26:02 +0000

    debug #7 authing

--- functions/share-notes/src/main.js
+++ functions/share-notes/src/main.js
@@ -51,9 +51,8 @@ export default async ({ req, res, log, error }) => {
 
   if(req.method == "POST"){
     try{
-      return res.send("testing...")
       const response = await databases.getDocument(process.env.VITE_DATABASE_ID,process.env.VITE_NOTES_COLLECTION_ID,req.query.noteId)
-
+      return res.json({response:response})
 
       let perms = response.$permissions;
 
