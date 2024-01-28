481eb7009b3404fb9ec29f09f4e43030218a32d8
t4zzlerdeveloper <38925744+t4zzlerdeveloper@users.noreply.github.com>
2024-01-28 19:42:44 +0000

    small Note Editor v2 patches

--- src/views/NoteEditor/index.jsx
+++ src/views/NoteEditor/index.jsx
@@ -22,7 +22,7 @@ function NoteEditor(){
         },
         {
             type:"p",
-            text:"This is just some text that you can see as a <p/> object"
+            text:"This is just some <b>bold text</b> that you can see as a <p/> object"
         },
 
     ])
@@ -49,7 +49,6 @@ function NoteEditor(){
     }
 
     function handleKeyDown(e,index,type){
-
         
         if(e.key == "ArrowUp" && index >= 1){
             index--;
@@ -58,9 +57,10 @@ function NoteEditor(){
             index++;
         }
         else if(e.key == "Backspace"){
-            // e.preventDefault();
+            
             // index++;
-            if( document.getElementById("neid-"+index).innerHTML == "<br>"){
+            if( document.getElementById("neid-"+index).value == ""){
+                e.preventDefault();
                 removeBlock(index)
             }
           
@@ -70,44 +70,90 @@ function NoteEditor(){
             index++;
         }
         else if(e.key == "ArrowLeft" && index >= 1){
-            swapBlocks(index,index-1)
+            //swapBlocks(index,index-1)
         }
         else if(e.key == "ArrowRight" && index <= content.length-2){
-            swapBlocks(index,index+1)
+            //swapBlocks(index,index+1)
         }
         document.getElementById("neid-"+index).focus();
        
     }
 
 
+    function boldSelected(){
+        let a = document.getElementById("neid-"+currentBlockId)
+        var start =  a.selectionStart;
+        var end =  a.selectionEnd;
+        const cText = content[currentBlockId].text;
+        const left = cText.slice(0,start);
+        const middle = cText.slice(start,end);
+        const right= cText.slice(end,cText.length-1);
+
+        let newText = left + "<b>" + middle + "</b>" + right;
+
+        console.log([left,middle,right])
+        console.log(newText)
+     
+        content[currentBlockId].text = newText.replace("<b><b>","").replace("</b></b>","");
+        setFF(ff+1)
+    }
+
+    function updateContent(e,index){
+        let copy = content;
+        copy[index].text = e.target.value;
+        setContent(copy);
+        console.log(content)
+        setFF(ff+1)
+    }
+
+
     const [toolStyle,setToolStyle] = useState({});
+    const [currentBlockId,setCurrentBlockId] = useState(null);
 
     function handleMouseUp(){
+
+        let a = document.getElementById("neid-"+currentBlockId)
+        let left = a.selectionStart;
+        let width = a.selectionEnd-a.selectionStart;
+        console.log(left,width)
+    
         let selection = window.getSelection(),
-        getRange      = selection.getRangeAt(0),
-        selectionRect = getRange.getBoundingClientRect();
+        selectionRect = a.getBoundingClientRect();
          
-        if(getRange == 0) {setToolStyle({display:"none"}); return;}
+        if(width == 0) {setToolStyle({display:"none"}); return;}
     
        setToolStyle({
           top: selectionRect.top - 42 + 'px',
-          left: ( selectionRect.left + (selectionRect.width * 0.5)) + 'px'
+          left: ( selectionRect.left + (left * 7) +  (width * 3.5)) + 'px'
         });
     }
 
     return (<div className='note-editor'>
         {ff > -1 && content.map((c,index)=>{
-            return<> <c.type 
+            return<> <input className={c.type}
                 style={{textDecoration:c.underline ? "underline" : "",color:c.color || ""}} 
                 id={"neid-" + index} 
-                onMouseMove={()=>{handleMouseUp()}}
+                onFocus={()=>{setCurrentBlockId(index)}}
+                onSelectCapture={()=>{handleMouseUp();}}
                 onKeyDown={(e)=>{handleKeyDown(e,index,c.type)}} 
-                contentEditable={true} dangerouslySetInnerHTML={{__html: c.text || ""}}>
+                onChange={(e)=>{updateContent(e,index)}}                
+                value={c.text}/>
+                </>
+
+            return<> <c.type
+                style={{textDecoration:c.underline ? "underline" : "",color:c.color || ""}} 
+                id={"neid-" + index} 
+                onFocus={()=>{setCurrentBlockId(index)}}
+                onMouseMove={()=>{handleMouseUp();}}
+                onKeyDown={(e)=>{handleKeyDown(e,index,c.type)}} 
+                onKeyUp={(e)=>{updateContent(e,index)}}
+                contentEditable={true} 
+                dangerouslySetInnerHTML={{__html: c.text || ""}}>
                 </c.type>
                 </>
         })}
         <div style={toolStyle} class="toolbar">
-            <img src={formatH1}/>
+            <img src={formatH1} onClick={()=>{boldSelected()}}/>
             <img src={formatH2}/>
             <img src={formatP}/>
         </div>
