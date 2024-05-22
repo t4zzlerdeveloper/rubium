3eaa45de954ddb953fddbc941deb0b820778852a
t4zzlerdeveloper <38925744+t4zzlerdeveloper@users.noreply.github.com>
2024-05-22 15:41:03 +0100

    test #3 python

--- functions/export-pdf/src/main.py
+++ functions/export-pdf/src/main.py
@@ -24,26 +24,3 @@ def main(context):
             "result": str(result),
         }
     )
-
-
-    # # You can log messages to the console
-    # context.log("Hello, Logs!")
-
-    # # If something goes wrong, log an error
-    # context.error("Hello, Errors!")
-
-    # # The `ctx.req` object contains the request data
-    # if context.req.method == "GET":
-    #     # Send a response with the res object helpers
-    #     # `ctx.res.send()` dispatches a string back to the client
-    #     return context.res.send("Hello, World!")
-
-    # # `ctx.res.json()` is a handy helper for sending JSON
-    # return context.res.json(
-    #     {
-    #         "motto": "Build like a team of hundreds_",
-    #         "learn": "https://appwrite.io/docs",
-    #         "connect": "https://appwrite.io/discord",
-    #         "getInspired": "https://builtwith.appwrite.io",
-    #     }
-    )
